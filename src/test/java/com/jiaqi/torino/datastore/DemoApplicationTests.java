dea2740fcc1b0b7b980001fda8b6cda0c150a4f8
Jiaqi Guo <jiaqguo@cisco.com>
2020-02-25 21:49:00 +0800

    Add: Init commit include dependencies

---
+++ src/test/java/com/jiaqi/torino/datastore/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaqi.torino.datastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
